55dcc986a44154d150275fd24add4bbe40cf8768
Virendra <virendrapal10081996@gmail.com>
2023-12-16 21:39:36 +0530

    react peoject

---
+++ src/App.js
@@ -0,0 +1,41 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-primary text-white text-center py-5">
+        <h1>Topics covered</h1>
+      </header>
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col ">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting </p>
+          </div>
+            <a href="https://www.geekster.in/" className="btn btn-primary">
+              Geeting started web
+            </a>
+          <div className="col">
+            <p>Lorem Ipsum is simok a gallcrambled it to make a type specimen book. It has survived not only five centuries, but also the leap i</p>
+          </div>
+          <div className="col">
+          <a href="https://www.geekster.in/" className="btn btn-primary">
+              HTML web
+            </a>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap ip i</p>
+          </div>
+
+          <div className="col">
+          <a href="https://www.geekster.in/" className="btn btn-primary">
+             CSS web
+            </a>
+            <p>LoLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap i i</p>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
